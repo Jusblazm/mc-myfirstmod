c9bbf2401418e4e1d2602f8b86df845d058b4a98
Jusblazm <ryanlarkin0@gmail.com>
2026-07-20 09:49:23 -0700

    video 4: creative mode tab

--- src/main/java/com/jusblazm/myfirstmod/MyFirstMod.java
+++ src/main/java/com/jusblazm/myfirstmod/MyFirstMod.java
@@ -1,6 +1,7 @@
 package com.jusblazm.myfirstmod;
 
 import com.jusblazm.myfirstmod.block.ModBlocks;
+import com.jusblazm.myfirstmod.item.ModCreativeModeTabs;
 import com.jusblazm.myfirstmod.item.ModItems;
 import net.minecraft.world.item.CreativeModeTabs;
 import org.slf4j.Logger;
@@ -33,6 +34,8 @@ public class MyFirstMod {
         // Note that this is necessary if and only if we want *this* class (ExampleMod) to respond directly to events.
         // Do not add this line if there are no @SubscribeEvent-annotated functions in this class, like onServerStarting() below.
         NeoForge.EVENT_BUS.register(this);
+
+        ModCreativeModeTabs.register(modEventBus);
 
         ModItems.register(modEventBus);
         ModBlocks.register(modEventBus);
