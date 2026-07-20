aa819f4cb49fa82b8a1b85287602d7bf6133c91c
Jusblazm <ryanlarkin0@gmail.com>
2026-07-20 12:23:53 -0700

    video 9: custom tooltips

--- src/main/java/com/jusblazm/myfirstmod/item/ModItems.java
+++ src/main/java/com/jusblazm/myfirstmod/item/ModItems.java
@@ -3,10 +3,15 @@ package com.jusblazm.myfirstmod.item;
 import com.jusblazm.myfirstmod.MyFirstMod;
 import com.jusblazm.myfirstmod.item.custom.ChiselItem;
 import com.jusblazm.myfirstmod.item.custom.FuelItem;
+import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.neoforge.registries.DeferredItem;
 import net.neoforged.neoforge.registries.DeferredRegister;
+
+import java.util.List;
 
 public class ModItems {
     public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(MyFirstMod.MOD_ID);
@@ -23,7 +28,13 @@ public class ModItems {
 
     public static final DeferredItem<Item> RADISH = ITEMS.register("radish",
             () -> new Item(new Item.Properties()
-                    .food(ModFoodProperties.RADISH)));
+                    .food(ModFoodProperties.RADISH)) {
+                @Override
+                public void appendHoverText(ItemStack stack, TooltipContext context, List<Component> tooltipComponents, TooltipFlag tooltipFlag) {
+                    tooltipComponents.add(Component.translatable("tooltip.myfirstmod.radish.tooltip"));
+                    super.appendHoverText(stack, context, tooltipComponents, tooltipFlag);
+                }
+            });
 
     public static final DeferredItem<Item> FROSTFIRE_ICE = ITEMS.register("frostfire_ice",
             () -> new FuelItem(new Item.Properties(), 800));
