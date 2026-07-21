32f37190a1013a8d92ddb2713e7bf257ca6a3d05
Jusblazm <ryanlarkin0@gmail.com>
2026-07-21 15:26:22 -0700

    video 33: basic world gen setup

--- src/main/java/com/jusblazm/myfirstmod/datagen/ModDatapackProvider.java
+++ src/main/java/com/jusblazm/myfirstmod/datagen/ModDatapackProvider.java
@@ -4,11 +4,15 @@ import com.jusblazm.myfirstmod.MyFirstMod;
 import com.jusblazm.myfirstmod.enchantment.ModEnchantments;
 import com.jusblazm.myfirstmod.trim.ModTrimMaterials;
 import com.jusblazm.myfirstmod.trim.ModTrimPatterns;
+import com.jusblazm.myfirstmod.worldgen.ModBiomeModifiers;
+import com.jusblazm.myfirstmod.worldgen.ModConfiguredFeatures;
+import com.jusblazm.myfirstmod.worldgen.ModPlacedFeatures;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.core.RegistrySetBuilder;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.data.PackOutput;
 import net.neoforged.neoforge.common.data.DatapackBuiltinEntriesProvider;
+import net.neoforged.neoforge.registries.NeoForgeRegistries;
 
 import java.util.Set;
 import java.util.concurrent.CompletableFuture;
@@ -17,7 +21,11 @@ public class ModDatapackProvider extends DatapackBuiltinEntriesProvider {
     public static final RegistrySetBuilder BUILDER = new RegistrySetBuilder()
             .add(Registries.TRIM_MATERIAL, ModTrimMaterials::bootstrap)
             .add(Registries.TRIM_PATTERN, ModTrimPatterns::bootstrap)
-            .add(Registries.ENCHANTMENT, ModEnchantments::bootstrap);
+            .add(Registries.ENCHANTMENT, ModEnchantments::bootstrap)
+
+            .add(Registries.CONFIGURED_FEATURE, ModConfiguredFeatures::bootstrap)
+            .add(Registries.PLACED_FEATURE, ModPlacedFeatures::bootstrap)
+            .add(NeoForgeRegistries.Keys.BIOME_MODIFIERS, ModBiomeModifiers::bootstrap);
 
     public ModDatapackProvider(PackOutput output, CompletableFuture<HolderLookup.Provider> registries) {
         super(output, registries, BUILDER, Set.of(MyFirstMod.MOD_ID));
