31815d28dbd851aa5e1fab9de2eb429f31eb294b
Jusblazm <ryanlarkin0@gmail.com>
2026-07-20 21:22:52 -0700

    video 23: understanding events

--- src/main/java/com/jusblazm/myfirstmod/event/ModEvents.java
+++ src/main/java/com/jusblazm/myfirstmod/event/ModEvents.java
@@ -52,7 +52,7 @@ public class ModEvents {
     public static void livingDamage(LivingDamageEvent.Pre event) {
         if(event.getEntity() instanceof Sheep sheep && event.getSource().getDirectEntity() instanceof Player player) {
             if(player.getMainHandItem().getItem() == Items.END_ROD) {
-                player.sendSystemMessage(Component.literal(player.getName().getString() + " just hit a sheep with an END ROD? You SICK FREAK!"));
+                player.sendSystemMessage(Component.literal(player.getName().getString() + " just hit a sheep with an END ROD? YOU SICK FREAK!"));
                 sheep.addEffect(new MobEffectInstance(MobEffects.POISON, 600, 6));
                 player.getMainHandItem().shrink(1);
             }
