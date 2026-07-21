25f7bc2e1554b9b12b563bad5d704a96c0ae5286
Jusblazm <ryanlarkin0@gmail.com>
2026-07-21 12:39:06 -0700

    video 27: custom potions

--- src/main/java/com/jusblazm/myfirstmod/MyFirstMod.java
+++ src/main/java/com/jusblazm/myfirstmod/MyFirstMod.java
@@ -5,6 +5,7 @@ import com.jusblazm.myfirstmod.component.ModDataComponents;
 import com.jusblazm.myfirstmod.effect.ModEffects;
 import com.jusblazm.myfirstmod.item.ModCreativeModeTabs;
 import com.jusblazm.myfirstmod.item.ModItems;
+import com.jusblazm.myfirstmod.potion.ModPotions;
 import com.jusblazm.myfirstmod.sound.ModSounds;
 import com.jusblazm.myfirstmod.util.ModItemProperties;
 import net.minecraft.world.item.CreativeModeTabs;
@@ -50,6 +51,7 @@ public class MyFirstMod {
         ModSounds.register(modEventBus);
 
         ModEffects.register(modEventBus);
+        ModPotions.register(modEventBus);
 
         // Register the item to a creative tab
         modEventBus.addListener(this::addCreative);
